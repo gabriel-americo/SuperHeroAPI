112249066b44a251a0b131af14a92d2d6db8f7ae
Gabriel Americo <gabrielamerico90@gmail.com>
2023-06-15 20:41:26 -0300

    SuperHero API Upload Project

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> superHeroes = new List<SuperHero>
+            {
+                new SuperHero
+                {   Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Malibu"
+                },
+                new SuperHero
+                {   Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            return Ok(superHeroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetOneHeroes(int id)
+        {
+            var hero = superHeroes.FirstOrDefault(x => x.Id == id);
+
+            if (hero == null)
+                return NotFound("Sorry, but this hero doen't exist.");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            superHeroes.Add(hero);
+            return Ok(superHeroes);
+        }
+    }
+}
